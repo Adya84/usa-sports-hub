9ec683c83f6c915f280315600dd23a57c8062216
Ady <94404576+Adya84@users.noreply.github.com>
2026-09-24 00:58:39 +0100

    Bind USA sports dashboard to sensor data

--- tests/usa_sports_panel.test.cjs
+++ tests/usa_sports_panel.test.cjs
@@ -4,7 +4,7 @@ const test = require('node:test');
 
 test('USA panel defines the sport selector and shared inner tabs', () => {
   const source = fs.readFileSync('custom_components/usa_sports_hub/frontend/usa-sports-hub-panel-v2.js', 'utf8');
-  for (const value of ['NFL', 'NBA', 'MLB', 'NHL', 'MLS', 'More Sports', 'Overview', 'Standings', 'My Team', 'background-nfl.png', 'background-nba.png', 'background-mlb.png', 'background-nhl.png', 'background-mls.png', 'background-more.png', 'KEEP US IN PLAY', 'UP TO DATE', 'COUNTRY', 'LEAGUE', 'TEAM', 'data-country', 'data-team']) {
+  for (const value of ['NFL', 'NBA', 'MLB', 'NHL', 'MLS', 'More Sports', 'Overview', 'Standings', 'My Team', 'background-nfl.png', 'background-nba.png', 'background-mlb.png', 'background-nhl.png', 'background-mls.png', 'background-more.png', 'KEEP US IN PLAY', 'UP TO DATE', 'COUNTRY', 'LEAGUE', 'TEAM', 'data-country', 'data-team', 'sensor.usa_sports_hub_', 'fixtures', 'results', 'standings', 'news']) {
     assert.match(source, new RegExp(value));
   }
   assert.doesNotMatch(source, /Last Man Standing|Acca League/);
