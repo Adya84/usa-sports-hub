dc88335e2cac4f7b10bf6be3f2f200038c48df73
Ady <94404576+Adya84@users.noreply.github.com>
2026-09-25 00:41:18 +0100

    Remove unused legacy Football Hub code

--- custom_components/usa_sports_hub/data/__init__.py
+++
@@ -1,7 +0,0 @@
-"""USA Sports Hub data helpers."""
-
-from .fixtures import get_fixtures
-from .live import get_live_matches
-from .players import get_players
-from .standings import get_standings
-from .top_scorers import get_top_scorers
