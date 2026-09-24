66a767bcea64d76c2e37ec7598ac38a41d485afe
Ady <94404576+Adya84@users.noreply.github.com>
2026-09-25 00:42:45 +0100

    Remove stale unused Football Hub constants

--- custom_components/usa_sports_hub/const.py
+++ custom_components/usa_sports_hub/const.py
@@ -1,11 +1,5 @@
 """Constants for USA Sports Hub."""
 
 DOMAIN = "usa_sports_hub"
-
 NAME = "USA Sports Hub"
-
 VERSION = "0.0.4-beta.4"
-
-DEFAULT_SCAN_INTERVAL = 60
-
-API_BASE = "https://v3.football.api-sports.io"
